ea29d6c99b087bdb79c168130530b772287134e8
Vanessa-byt <24308060610012@cetis61.edu.mx>
2026-05-13 12:28:21 -0500

    hola

--- app.py
+++ app.py
@@ -4,10 +4,9 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client["usuariosDB"]                
-usuarios_collection = db["usuarios"]     
-tareas_collection = db["tareas"]
+client = MongoClient("mongodb+srv://ricardorosal7335_db_user:kiraymoster39@cluster0.ixvdcur.mongodb.net/")
+db = client["Recetario"]
+usuarios_collection = db["usuarios"]
 
 
 def obtener_usuario(email: str, contraseña: str) -> Optional[Dict]:
@@ -16,6 +15,22 @@ def obtener_usuario(email: str, contraseña: str) -> Optional[Dict]:
         usuario['_id'] = str(usuario['_id'])
     return usuario
 
+@app.route("/")
+def inicio():
+    return render_template("nicio.html")
 
+@app.route("/login", methods=["POST"])
+def login():
+
+    email = request.form.get("email")
+    contraseña = request.form.get("contraseña")
+
+    usuario = obtener_usuario(email, contraseña)
+
+    if usuario:
+        return redirect(url_for("recetario"))
+    else:
+        return render_template("nicio.html", mensaje="Usuario o contraseña incorrectos")
+connect = ChefEnProceso(client)
 if __name__ == "__main__":
     app.run(debug=True)
